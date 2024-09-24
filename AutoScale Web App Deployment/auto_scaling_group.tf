1daff50efb23ca59f56c66d67d3f030da5a628ff
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-09-24 21:02:24 +0300

    final edit

--- AutoScale Web App Deployment/auto_scaling_group.tf
+++ AutoScale Web App Deployment/auto_scaling_group.tf
@@ -6,34 +6,43 @@ resource "aws_launch_configuration" "app" {
   key_name               = aws_key_pair.UbuntuKP.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   security_groups        = [aws_security_group.HTTP-SG.id]
-  
+
   user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    yum install -y python3 aws-cli
+    # Update and install necessary packages
+    sudo apt update -y
+    sudo apt install -y python3 awscli
 
+    # Verify AWS CLI installation
+    aws --version
+
+    # Get the instance ID and log instance details
     INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-    echo "Instance ID: \$INSTANCE_ID" > /var/log/instance_info.log
-    echo "Hostname: \$(hostname -f)" >> /var/log/instance_info.log
-    echo "Date: \$(date)" >> /var/log/instance_info.log
+    HOSTNAME=$(hostname -f)
+
+    echo "Instance ID: \$INSTANCE_ID" | sudo tee /var/log/instance_info.log
+    echo "Hostname: \$HOSTNAME" | sudo tee -a /var/log/instance_info.log
+    echo "Date: \$(date)" | sudo tee -a /var/log/instance_info.log
 
     # Install Nginx
-    yum install -y nginx
+    sudo apt install -y nginx
 
-    # Create HTML file with your name
-    echo "<html><body><h1>Hello, From Mahmoud Sharara Server</h1></body></html>" > /var/www/html/index.html
+    # Create an HTML file with your name and instance information
+    echo "<html><body><h1>Hello, From Mahmoud Sharara Server</h1><p>Instance ID: \$INSTANCE_ID</p><p>Hostname: \$HOSTNAME</p></body></html>" | sudo tee /var/www/html/index.html
 
-    # Start Nginx
-    systemctl start nginx
-    systemctl enable nginx
+    # Start and enable Nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
 
-    # Upload log file to S3 periodically
+    # Upload the log file to S3 periodically
     while true; do
       aws s3 cp /var/log/instance_info.log s3://cloudkode-project-sharara/instance_logs/\$(hostname)-instance_info.log
       sleep 30 # Upload every 30 seconds
     done
   EOF
 }
+
+
 
 
 # Create Auto Scaling Group (ASG)
